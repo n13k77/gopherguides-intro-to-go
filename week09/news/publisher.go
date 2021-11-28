9943e8c904f5b39c628e5ef6b581fe6c2bfde868
Niek Brom <niekbrom@gmail.com>
2021-11-28 20:17:42 +0100

    additional functionality for publisher

--- week09/news/publisher.go
+++ week09/news/publisher.go
@@ -1,44 +1,97 @@
 package news
 
 import (
+	"os"
 	"sync"
 )
 
 // type Publisher publishes news articles to which can be subscribed
 type Publisher struct {
-	mutex  	sync.RWMutex
-	subs   	map[string]map[int]chan Article
-	stopped	bool
-	state	string // location of the state file 
+	mutex  		sync.RWMutex
+	subs   		map[string]map[int]chan Article
+	stopped		bool
+	backupfile 	*os.File
+	publishfile	*os.File // this is part of optional functionality, check for nil before using
+	jsonformat	bool
 }
 
 type PublisherConfig struct {
 	backupfile	string
-
-
+	publishfile	string
+	jsonformat	bool
 }
+
 // func NewPublisher() creates a new instance of a Publisher
-func NewPublisher(state string) (*Publisher, error) {
+func NewPublisher(config PublisherConfig) (*Publisher, error) {
+	var err error
+
 	p := &Publisher{}
 	p.subs = make(map[string]map[int]chan Article)
 	p.stopped = false
-	p.state = "" // TODO
-	return p, nil // TODO, return an error in case the statefile cannot be written to
+
+	// if not specified, set backupfile to a default location
+	if config.backupfile == "" {
+		config.backupfile = "./tmp.txt"
+	}
+
+	// check writability of file locations
+    p.backupfile, err = os.Create(config.backupfile)
+    if err != nil {
+        return nil, err
+    }
+
+	if config.publishfile != "" {
+		p.jsonformat = config.jsonformat
+		p.publishfile, err = os.Create(config.publishfile)
+	}
+
+	if err != nil {
+        return nil, err
+    }
+
+    defer func() {
+        if err := p.backupfile.Close(); err != nil {
+            panic(err)
+        }
+    }()
+
+	return p, nil
 }
 
-// func Subscriber() adds a subscriber to a publisher. The subscriber has to
+// func Subscribe() adds a subscriber to a publisher. The subscriber has to
 // provide the topic to which it is subscribing and its unique identifier
-
-// TODO: enable subscribing to multiple topics
-// TODO: ensure that the subscriber is subscribing with a truly unique ID
-// TODO: enable subscribing to multiple or all topics at once
-func (p *Publisher) Subscribe(category string, id int) <-chan Article {
+func (p *Publisher) Subscribe(id int, categories ...string) (<-chan Article, error) {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
 
 	ch := make(chan Article, 1)
-	p.subs[category][id] = ch
-	return ch
+
+	for _, category := range categories {
+		p.subs[category][id] = ch
+	}
+	return ch, nil
+}
+
+// func Unsubscriber() removes a subscriber from a publisher. The subscriber has to
+// provide the topic from which it is unsubscribing and its unique identifier
+func (p *Publisher) Unsubscribe(id int, categories ...string) {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	cats := categories
+
+	// if no categories are provided, remove all subscriptions for this subscriber
+	if len(cats) == 0 {
+		i := 0
+		for k := range(p.subs) {
+			cats[i] = k
+			i++
+		}
+	}
+
+	for _, cat := range cats {
+		delete(p.subs[cat], id) // TODO: test for deletion in unsubscribed channel
+	}
 }
 
 // func Publish publishes an article to the subscribers subscribed to that 
@@ -69,6 +122,10 @@ func (p *Publisher) Close() {
 			}
 		}
 	}
+}
+
+func (p *Publisher) Stopped() bool {
+	return p.stopped
 }
 
 // func Save saves the state of the publisher to the configured saving location 
--- week09/news/subscribers.go
+++ week09/news/subscribers.go
@@ -1,1 +1,14 @@
 package news
+
+import (
+	"time"
+)
+
+type Subscriber struct {
+	id 		int
+}
+
+func NewSubscriber () {
+	s := &Subscriber{}
+	s.id = int(time.Now().UnixMicro()) // unique ID for subscriber
+}
